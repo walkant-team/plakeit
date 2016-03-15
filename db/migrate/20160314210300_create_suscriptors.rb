08e6f40b6ed45dda1719f411817f1ef81e3081ec
angelsystem <angelsystem1387@gmail.com>
2016-03-14 19:05:41 -0500

    add type to migrate

--- db/migrate/20160314210300_create_suscriptors.rb
+++ db/migrate/20160314210300_create_suscriptors.rb
@@ -2,6 +2,7 @@ class CreateSuscriptors < ActiveRecord::Migration
   def change
     create_table :suscriptors do |t|
       t.string :email
+      t.string :type
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20160314210300) do
 
   create_table "suscriptors", force: :cascade do |t|
     t.string   "email"
+    t.string   "type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
