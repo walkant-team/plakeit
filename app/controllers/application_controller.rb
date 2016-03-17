95a4d7d3cda6580fdab3bcedc42510fa0a79130a
angelsystem <angelsystem1387@gmail.com>
2016-03-17 11:30:07 -0500

    update language detect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,13 @@ class ApplicationController < ActionController::Base
   before_action :set_locale
   protect_from_forgery with: :exception
 
+
   def set_locale
-    I18n.locale = params[:locale]
+    I18n.locale = params[:locale] || extract_locale_from_accept_language_header
   end
+
+  private
+    def extract_locale_from_accept_language_header
+      request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+    end
 end
