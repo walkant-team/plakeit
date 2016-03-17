0891b0c5574cdad6834d89c4954f0a19d3dfb375
angelsystem <angelsystem1387@gmail.com>
2016-03-17 11:17:14 -0500

    update align

--- app/mailers/suscriptor_mailer.rb
+++ app/mailers/suscriptor_mailer.rb
@@ -2,7 +2,7 @@ class SuscriptorMailer < ApplicationMailer
 
   def suscription_business(suscriptor)
     @suscriptor = suscriptor
-    mail to: @suscriptor.email, subject: 'Bienvenido a Plakeit para Empresas'
+    mail to: @suscriptor.email, subject: 'Bienvenido a Plakeit para Negocios'
   end
 
   def suscription_customer(suscriptor)
