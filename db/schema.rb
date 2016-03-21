afdc759086b2af936d655b2b9c75447e7805279c
Jorge Crisóstomo Palacios <videmor@gmail.com>
2016-03-21 16:26:10 -0500

    new model events

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160314210300) do
+ActiveRecord::Schema.define(version: 20160321212528) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "start_at"
+    t.string   "address"
+    t.string   "location"
+    t.float    "latitude"
+    t.float    "longitude"
+    t.text     "description"
+    t.string   "image"
+    t.integer  "count_views"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "suscriptors", force: :cascade do |t|
     t.string   "email"
