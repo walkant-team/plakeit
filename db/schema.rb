04c37c6074ee7f606f9a495e3dd56589532eef36
videmor <videmor@gmail.com>
2016-03-29 15:46:57 -0500

    delete migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,33 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160321212528) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "events", force: :cascade do |t|
-    t.string   "title"
-    t.datetime "start_at"
-    t.string   "address"
-    t.string   "location"
-    t.float    "latitude"
-    t.float    "longitude"
-    t.text     "description"
-    t.string   "image"
-    t.integer  "count_views"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
-  create_table "suscriptors", force: :cascade do |t|
-    t.string   "email"
-    t.string   "fullname"
-    t.string   "phone"
-    t.string   "name_business"
-    t.string   "type"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-  end
 
 end
