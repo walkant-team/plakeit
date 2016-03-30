c6beaca3956283f19f638fd63e9d056c5790ab43
morales.saldarriaga <morales.saldarriaga@gmail.com>
2016-03-30 13:10:38 -0500

    new views plakeit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   get 'events/index'
+  get 'events/show'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
