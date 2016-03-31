e2eb8ca4ca99b3bee60a7a8b9869c0a991a88701
angelsystem <angelsystem1387@gmail.com>
2016-03-31 15:43:48 -0500

    add loading infinite

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @public_events = PublicEvent.all
+    @public_events = PublicEvent.order(:start_at).page(params[:page]).per(10)
   end
 
   def show
