8190a4871e9400543b3419003aa4d1bff8cf8b49
videmor <videmor@gmail.com>
2016-04-13 17:00:10 -0500

    Rubocop - Rails enabled :octocat:

--- app/models/public_event.rb
+++ app/models/public_event.rb
@@ -1,7 +1,7 @@
 class PublicEvent < Event
   is_impressionable
 
-  scope :weekly, -> { where('start_at <= ?', Date.today.end_of_week) }
-  scope :not_weekly, -> { where('start_at > ?', Date.today.end_of_week) }
+  scope :weekly, -> { where('start_at <= ?', Time.zone.end_of_week) }
+  scope :not_weekly, -> { where('start_at > ?', Time.zone.end_of_week) }
   scope :by_category, ->(category) { where(category: category) if category.present? }
 end
