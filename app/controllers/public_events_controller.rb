612ca9fbcffd666a3bc0baeec5f9898a1073114c
videmor <videmor@gmail.com>
2016-04-14 18:21:29 -0500

    Add google maps :octocat:

--- app/controllers/public_events_controller.rb
+++ app/controllers/public_events_controller.rb
@@ -3,5 +3,10 @@ class PublicEventsController < ApplicationController
 
   def show
     @public_event = PublicEvent.find(params[:id])
+    @hash = Gmaps4rails.build_markers(@public_event) do |event, marker|
+      marker.lat event.latitude
+      marker.lng event.longitude
+      marker.infowindow "<h6>#{event.location}</h6>"
+    end
   end
 end
