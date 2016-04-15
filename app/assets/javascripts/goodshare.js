0e27ebe751846763a33c9b279aa9866df0a367a2
Davis Benito <Davis@MacBook-Air-de-Davis.local>
2016-04-14 19:14:32 -0500

    network social

--- app/assets/javascripts/goodshare.js
+++ app/assets/javascripts/goodshare.js
@@ -1,27 +1,6 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-//= require jquery
-//= require jquery_ujs
-//= require base/modernizr.min
-//= require materialize-sprockets
-//= require turbolinks
-//= require underscore
-//= require gmaps/google
-//= require_self
-
 (function($, window, document, undefined) {
 
-
+alert("ssss");
 $(document).ready(function() {
 /*
 * Variables
@@ -45,11 +24,11 @@ init: function(_element, _options) {
 * image = current document <meta property="og:image" ... />
 */
 var self = goodshare, options = $.extend({
-type: 'vk',
-url:  location.href,
-title:  metaOTitle,
+type:	'vk',
+url:	location.href,
+title:	metaOTitle,
 text: metaOGDescription,
-image:  metaOGImage
+image:	metaOGImage
 }, $(_element).data(), _options);
 /*
 * Open popup
@@ -317,166 +296,3 @@ goodshare.init(this);
 $(document).getCount();
 });
 })(jQuery, window, document);
-
-$(function() {
-
-
-  $('select').material_select();
-  $('.parallax').parallax();
-  // maps
-  var mapStyle = [
-      {
-          "featureType": "all",
-          "elementType": "labels.text.fill",
-          "stylers": [
-              {
-                  "color": "#ffffff"
-              }
-          ]
-      },
-      {
-          "featureType": "all",
-          "elementType": "labels.text.stroke",
-          "stylers": [
-              {
-                  "color": "#000000"
-              },
-              {
-                  "lightness": 13
-              }
-          ]
-      },
-      {
-          "featureType": "administrative",
-          "elementType": "geometry.fill",
-          "stylers": [
-              {
-                  "color": "#000000"
-              }
-          ]
-      },
-      {
-          "featureType": "administrative",
-          "elementType": "geometry.stroke",
-          "stylers": [
-              {
-                  "color": "#144b53"
-              },
-              {
-                  "lightness": 14
-              },
-              {
-                  "weight": 1.4
-              }
-          ]
-      },
-      {
-          "featureType": "landscape",
-          "elementType": "all",
-          "stylers": [
-              {
-                  "color": "#08304b"
-              }
-          ]
-      },
-      {
-          "featureType": "poi",
-          "elementType": "geometry",
-          "stylers": [
-              {
-                  "color": "#0c4152"
-              },
-              {
-                  "lightness": 5
-              }
-          ]
-      },
-      {
-          "featureType": "road.highway",
-          "elementType": "geometry.fill",
-          "stylers": [
-              {
-                  "color": "#000000"
-              }
-          ]
-      },
-      {
-          "featureType": "road.highway",
-          "elementType": "geometry.stroke",
-          "stylers": [
-              {
-                  "color": "#0b434f"
-              },
-              {
-                  "lightness": 25
-              }
-          ]
-      },
-      {
-          "featureType": "road.arterial",
-          "elementType": "geometry.fill",
-          "stylers": [
-              {
-                  "color": "#000000"
-              }
-          ]
-      },
-      {
-          "featureType": "road.arterial",
-          "elementType": "geometry.stroke",
-          "stylers": [
-              {
-                  "color": "#0b3d51"
-              },
-              {
-                  "lightness": 16
-              }
-          ]
-      },
-      {
-          "featureType": "road.local",
-          "elementType": "geometry",
-          "stylers": [
-              {
-                  "color": "#000000"
-              }
-          ]
-      },
-      {
-          "featureType": "transit",
-          "elementType": "all",
-          "stylers": [
-              {
-                  "color": "#146474"
-              }
-          ]
-      },
-      {
-          "featureType": "water",
-          "elementType": "all",
-          "stylers": [
-              {
-                  "color": "#021019"
-              }
-          ]
-      }
-  ];
-
-  var handler = Gmaps.build('Google');
-  handler.buildMap({
-      internal: {id: 'custom_style'},
-      provider: {
-        zoom:      15,
-        center:    new google.maps.LatLng(53.385873, -1.471471),
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        styles:    mapStyle,
-        lat:       -12.118071,
-        lng:       -76.997861,
-        scrollwheel:  false
-      }
-    },
-    function(){ }
-  );
-
-})
-
