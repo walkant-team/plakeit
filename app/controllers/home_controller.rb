4f9f53cd5dd40db00fd22d5ea7d7438a7f550013
videmor <videmor@gmail.com>
2016-04-16 18:18:27 -0500

    script pronto

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,8 @@
 class HomeController < ApplicationController
   def index
-    events = PublicEvent.all.order(:start_at)
+      events = PublicEvent.all.order(:start_at)
     category = Category.find_by(name: params[:category]) if params[:category]
     @public_events = events.not_weekly.by_category(category).page(params[:page]).per(9)
-      @weekly_events = events.weekly.by_category(category)
+    @weekly_events = events.weekly.by_category(category)
   end
 end
