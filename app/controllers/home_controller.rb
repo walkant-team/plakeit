0208f0b7b900508919b82f0d9c745c2794d06637
videmor <videmor@gmail.com>
2016-04-16 17:03:22 -0500

    Configurarion CircleCi with pronto

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,6 @@ class HomeController < ApplicationController
     events = PublicEvent.all.order(:start_at)
     category = Category.find_by(name: params[:category]) if params[:category]
     @public_events = events.not_weekly.by_category(category).page(params[:page]).per(9)
-    @weekly_events = events.weekly.by_category(category)
+      @weekly_events = events.weekly.by_category(category)
   end
 end
