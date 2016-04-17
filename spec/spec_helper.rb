52ec09b76905a981dd11aeaa35fbdc1d13552216
videmor <videmor@gmail.com>
2016-04-17 01:25:24 -0500

    upgrade database cleaner

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,29 +23,6 @@ RSpec.configure do |config|
   # assertion/expectation library such as wrong or the stdlib/minitest
   config.include FactoryGirl::Syntax::Methods
 
-  seed_tables = %w(categories tags)
-
-  config.before(:suite) do
-    DatabaseCleaner.clean_with(:truncation)
-    Rails.application.load_seed
-  end
-
-  config.before(:each) do
-    DatabaseCleaner.strategy = :transaction
-  end
-
-  config.before(:each, js: true) do
-    DatabaseCleaner.strategy = :truncation, { except: seed_tables }
-  end
-
-  config.before(:each) do
-    DatabaseCleaner.start
-  end
-
-  config.after(:each) do
-    DatabaseCleaner.clean
-  end
-
   # assertions if you prefer.
   config.expect_with :rspec do |expectations|
     # This option will default to `true` in RSpec 4. It makes the `description`
